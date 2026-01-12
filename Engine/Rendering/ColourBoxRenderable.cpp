9e41fc7ff7219b16e0cbe5db0a2d1ac78d637017
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-01-12 03:29:27 +0000

    Added ColourBoxRenderable for ease of use

---
+++ Engine/Rendering/ColourBoxRenderable.cpp
@@ -0,0 +1,16 @@
+﻿#include "ColourBoxRenderable.h"
+ColourBoxRenderable::ColourBoxRenderable(Transform* transform, const sf::Color& colour): BaseRenderable(transform), colour(colour)
+{
+    shape.setSize(sf::Vector2f(1,1));
+}
+
+void ColourBoxRenderable::Render(sf::RenderWindow* RenderWindow)
+{
+    shape.setPosition(transform->GetPosition());
+    shape.setRotation(transform->GetRotation());
+    shape.setScale(transform->GetScale());
+
+    shape.setFillColor(colour);
+
+    BaseRenderable::Render(RenderWindow);
+}
---
+++ Engine/Rendering/ColourBoxRenderable.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "BaseRenderable.h"
+
+class ColourBoxRenderable:public BaseRenderable
+{
+public:
+    ColourBoxRenderable(Transform* transform, const sf::Color& colour);
+
+
+    void Render(sf::RenderWindow* RenderWindow) override;
+
+    sf::RectangleShape shape;
+    sf::Color colour;
+};
