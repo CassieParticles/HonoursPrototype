a347d8a52b1e2e826db2f0da9e0fdf2c7bd5c61f
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-01-26 15:34:29 +0000

    Created marching squares game obejct that can generate marching squares

---
+++ Project/MarchingSquares/MarchingSquaresObject.cpp
@@ -0,0 +1,66 @@
+﻿#include "../MarchingSquares/MarchingSquaresObject.h"
+MarchingSquaresObject::MarchingSquaresObject(float* data, int width, int height):voxelGrid(data,width,height)
+{
+    //Voxelgrid needs -ve values on it's edges
+    voxelGrid.AddBorder(-1.0f);
+}
+
+void MarchingSquaresObject::Render(sf::RenderWindow* window)
+{
+
+}
+
+void MarchingSquaresObject::Generate()
+{
+    int width = voxelGrid.getWidth();
+    int height = voxelGrid.getHeight();
+
+    triangles.clear();
+    for(int y=0;y<height - 1; ++y)
+    {
+        for(int x=0;x<width - 1; ++x)
+        {
+            //Get the values for each of the 4 corners
+            float cornerValues[4]
+            {
+                voxelGrid.getVoxel(x,y),
+                voxelGrid.getVoxel(x+1,y),
+                voxelGrid.getVoxel(x,y+1),
+                voxelGrid.getVoxel(x+1,y+1)
+            };
+
+            //Get which case the cell is
+            int index{};
+            for(int i=0;i<4;++i)
+            {
+                index += (cornerValues[i] > 0) << i;
+            }
+
+            //Get the indices of the case
+            const int* indexSet = indexTable[index];
+
+            //Get the vertex position of each corner
+            sf::Vector2f vertices[8];
+            vertices[0] = sf::Vector2f(static_cast<float>(x), static_cast<float>(y));
+            vertices[2] = sf::Vector2f(static_cast<float>(x + 1), static_cast<float>(y));
+            vertices[4] = sf::Vector2f(static_cast<float>(x + 1), static_cast<float>(y + 1));
+            vertices[6] = sf::Vector2f(static_cast<float>(x), static_cast<float>(y + 1));
+
+            //Calculate the vertex position of each midpoint (lerp between positions)
+            vertices[1] = findMidpointX(vertices[0],vertices[2],cornerValues[0],cornerValues[1]);
+            vertices[3] = findMidpointY(vertices[2],vertices[4],cornerValues[1],cornerValues[2]);
+            vertices[5] = findMidpointX(vertices[6],vertices[4],cornerValues[3],cornerValues[2]);
+            vertices[7] = findMidpointY(vertices[0],vertices[6],cornerValues[0],cornerValues[3]);
+
+            //Assemble triangles from indices using vertices
+            for(int i=0;indexSet[i] != 8;i+=3)
+            {
+                triangles.emplace_back(
+                    vertices[indexSet[i + 0]],
+                    vertices[indexSet[i + 1]],
+                    vertices[indexSet[i + 2]]
+                );
+            }
+        }
+    }
+}
---
+++ Project/MarchingSquares/MarchingSquaresObject.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <GameObjects/GameObject.h>
+
+#include "VoxelGrid.h"
+
+struct Triangle
+{
+    Triangle(const sf::Vector2f& a = {}, const sf::Vector2f& b = {}, const sf::Vector2f& c = {})
+        : A(a),
+          B(b),
+          C(c) {}
+
+    sf::Vector2f A;
+    sf::Vector2f B;
+    sf::Vector2f C;
+};
+
+class MarchingSquaresObject : public GameObject
+{
+public:
+    MarchingSquaresObject(float* data, int width, int height);
+    void Render(sf::RenderWindow* window) override;//Overwrite with new array//Overwrite with new array
+private:
+    void Generate();
+
+    VoxelGrid voxelGrid;
+    std::vector<Triangle> triangles;
+
+    //Indices of triangles in each case, 8 means no more triangles, CW order, starting at lowest
+    constexpr static int indexTable[16][10]
+    {
+        {8,8,8,8,8,8,8,8,8,8},//0
+        {0,7,1,8,8,8,8,8,8,8},
+        {1,3,2,8,8,8,8,8,8,8},
+        {0,7,3,0,3,2,8,8,8,8},
+        {3,5,4,8,8,8,8,8,8,8},//4
+        {0,7,1,3,5,4,8,8,8,8},
+        {1,4,2,1,5,4,8,8,8,8},
+        {0,7,2,2,7,5,2,5,4,8},
+        {5,7,6,8,8,8,8,8,8,8},//8
+        {0,5,1,0,6,5,8,8,8,8},
+        {1,3,2,5,7,6,8,8,8,8},
+        {0,3,2,0,5,3,0,6,5,8},
+        {3,7,4,4,7,6,8,8,8,8},//12
+        {0,6,1,1,6,3,3,6,4,8},
+        {1,4,2,1,7,4,4,7,6,8},
+        {0,4,2,0,6,4,8,8,8,8}
+    };
+
+    inline float normalise(float a, float b, float v) { return (v - a) / (b - a); }
+    inline sf::Vector2f findMidpointX(sf::Vector2f a, sf::Vector2f b, float aVal, float bVal) { return sf::Vector2f(a.x + normalise(aVal, bVal, 0) * (b.x - a.x), a.y); }
+    inline sf::Vector2f findMidpointY(sf::Vector2f a, sf::Vector2f b, float aVal, float bVal) { return sf::Vector2f(a.x, a.y + normalise(aVal, bVal, 0) * (b.y - a.y)); }
+};
