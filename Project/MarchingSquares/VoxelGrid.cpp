3b0f036674176e6a2c2730c1406f38e167c93a44
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-01-26 14:21:48 +0000

    Created voxel grid object for managing voxel grid of a game object

---
+++ Project/MarchingSquares/VoxelGrid.cpp
@@ -0,0 +1,82 @@
+﻿#include "VoxelGrid.h"
+VoxelGrid::VoxelGrid(): width(0),height(0) {}
+
+VoxelGrid::VoxelGrid(float* data, int width, int height, int x, int y): width(width), height(height), x(x), y(y)
+{
+    if(data!=nullptr)
+    {
+        voxelGrid = new float[width * height];
+        std::copy(data,data + width * height,voxelGrid);
+    }
+}
+
+VoxelGrid::~VoxelGrid() {}
+float *VoxelGrid::GetData() const {return voxelGrid;}
+float VoxelGrid::getVoxel(int x, int y) const {return voxelGrid[y * width + x];}
+float& VoxelGrid::operator[](int index) const {return voxelGrid[index];}
+
+void VoxelGrid::AddColumnLeft()
+{
+    float* newArr = new float[(width + 1) * height];
+
+    //Copy old data (one row at a time)
+    for(int row=0;row < height;row++)
+    {
+        newArr[(row + 1) * width] = 0.0f;
+        std::copy(voxelGrid + row * width, voxelGrid + (row + 1) * width, newArr + row * (width + 1) + 1);
+    }
+
+    //Overwrite with new array
+    delete[] voxelGrid;
+    voxelGrid = newArr;
+
+    width++;
+}
+
+void VoxelGrid::AddColumnRight()
+{
+    float* newArr = new float[(width + 1) * height];
+
+    //Copy old data(one row at a time)
+    for(int row=0;row<height;row++)
+    {
+        std::copy(voxelGrid + row * width, voxelGrid + (row + 1) * width, newArr + row * (width + 1));
+        newArr[(row + 1) * width - 1] = 0.0f;
+    }
+
+    //Overwrite with new array
+    delete[] voxelGrid;
+    voxelGrid = newArr;
+
+    width++;
+}
+
+void VoxelGrid::AddRowTop()
+{
+    //Create larger array
+    float* newArr = new float[width * (height + 1)];
+
+    //Copy old data into array (offset by width)
+    std::copy(voxelGrid,voxelGrid + (width * height), newArr + width);
+
+    //Overwrite with new array
+    delete[] voxelGrid;
+    voxelGrid = newArr;
+
+    height++;
+}
+
+void VoxelGrid::AddRowBottom()
+{
+    //Create larger array
+    float* newArr = new float[width * (height + 1)];
+
+    //Copy old data into array
+    std::copy(voxelGrid,voxelGrid + (width * height), newArr);
+
+    //Overwrite with new array
+    delete[] voxelGrid;
+    voxelGrid = newArr;
+
+    height++;
+}
---
+++ Project/MarchingSquares/VoxelGrid.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <vector>
+
+class VoxelGrid
+{
+public:
+    VoxelGrid();
+    VoxelGrid(float* data, int width, int height, int x=0, int y=0);
+
+    ~VoxelGrid();
+
+    float *GetData() const;
+
+    //Read values from the grid
+    float getVoxel(int x, int y) const;
+    float& operator[](int index) const;
+private:
+    void AddColumnLeft();
+    void AddColumnRight();
+    void AddRowTop();
+    void AddRowBottom();
+
+    float* voxelGrid;
+
+    //Width and height of the grid
+    int width;
+    int height;
+
+    //Local position of top left corner
+    int x;
+    int y;
+};
