fc0d355fcd4bfa9bcf4cfa17264fe8df00a7a395
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-02-05 18:51:26 +0000

    Created MS object

--- Project/MSNew/MSObject.h
+++ Project/MSNew/MSObject.h
@@ -2,22 +2,26 @@
 #include <vector>
 
 #include "../MarchingSquares/VoxelGrid.h"
+#include "Components/MSTrianglePhysics.h"
+#include "Components/MSTriangleRenderer.h"
 
-class MSComponent;
 class MSGenerator;
 
-class MSObject
+class MSObject: public GameObject
 {
 public:
     MSObject();
-
     void SetData(float* data, int width, int height);
-
     void Generate();
+
+    void Update() override;
+    void Render(sf::RenderWindow* window) override;
+
 protected:
     VoxelGrid grid;
 
     MSGenerator* generator;
 
-    std::vector<MSComponent> components;
+    MSTrianglePhysics physics;
+    MSTriangleRenderer renderer;
 };
