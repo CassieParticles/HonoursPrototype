698d9eca1f5638a0ce98df0bcb6c48d45db41ae7
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-03-10 16:56:48 +0000

    Added Line struct

---
+++ Project/MarchingSquares/Line.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <SFML/System/Vector2.hpp>
+
+
+struct Line
+{
+    Line(sf::Vector2f A = {}, sf::Vector2f B = {}):A(A),B(B) {}
+
+    friend bool operator==(const Line& lhs, const Line& rhs)
+    {
+        return lhs.A == rhs.A && lhs.B == rhs.B;
+    }
+    friend bool operator!=(const Line& lhs, const Line& rhs) { return !(lhs == rhs); }
+
+    sf::Vector2f A;
+    sf::Vector2f B;
+};
