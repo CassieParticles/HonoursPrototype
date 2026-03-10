610b92af7e4524ca29d6cdb2702666402b9e424c
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-03-10 17:40:46 +0000

    Implemented MarchingSquaresPhysicsDecimate able to collect lines

--- Project/MarchingSquares/Object/MarchingSquaresPhysicsDecimate.cpp
+++ Project/MarchingSquares/Object/MarchingSquaresPhysicsDecimate.cpp
@@ -1,1 +1,49 @@
 ﻿#include "MarchingSquaresPhysicsDecimate.h"
+
+#include <iostream>
+#include <Physics/PhysicsWorld.h>
+
+MarchingSquaresPhysicsDecimate::MarchingSquaresPhysicsDecimate(Transform* transform):transform(transform) {}
+MarchingSquaresPhysicsDecimate::~MarchingSquaresPhysicsDecimate()
+{
+    if(b2Body_IsValid(bodyId))
+    {
+        b2DestroyBody(bodyId);
+    }
+}
+
+
+
+void MarchingSquaresPhysicsDecimate::MSPhysicsBuilder::SetDynamic(bool isDynamic) {this->isDynamic=isDynamic;}
+void MarchingSquaresPhysicsDecimate::MSPhysicsBuilder::AddLine(Line line, int x, int y, int lineNo)
+{
+    int lineIndex = getIndex(x,y,lineNo);
+    lineArray[lineIndex] = line;
+    lineCount++;
+}
+
+void MarchingSquaresPhysicsDecimate::MSPhysicsBuilder::Build()
+{
+    b2BodyDef bodyDef = b2DefaultBodyDef();
+    bodyDef.position = object->transform->GetPositionb2();
+    bodyDef.rotation = object->transform->GetRotationb2();
+
+    bodyDef.type = isDynamic ? b2_dynamicBody : b2_staticBody;
+    object->bodyId = b2CreateBody(PhysicsWorld::GetWorldId(),&bodyDef);
+
+    b2ShapeDef shapeDef = b2DefaultShapeDef();
+    Line defaultLine = {};
+    int count{};
+}
+
+MarchingSquaresPhysicsDecimate::MSPhysicsBuilder::MSPhysicsBuilder(MarchingSquaresPhysicsDecimate* object, int gridWidth, int gridHeight)
+    :object(object),gridWidth(gridWidth),gridHeight(gridHeight),isDynamic(false),lineCount(0)
+{
+    //2 lines per cell
+    lineArray = new Line[gridWidth * gridHeight * 2];
+}
+
+MarchingSquaresPhysicsDecimate::MSPhysicsBuilder MarchingSquaresPhysicsDecimate::GetBuilder(int gridWidth, int gridHeight)
+{
+    return {this,gridWidth,gridHeight};
+}
--- Project/MarchingSquares/Object/MarchingSquaresPhysicsDecimate.h
+++ Project/MarchingSquares/Object/MarchingSquaresPhysicsDecimate.h
@@ -1,4 +1,42 @@
 ﻿#pragma once
+#include <GameObjects/Transform.h>
 
+#include "../Line.h"
 
-class MarchingSquaresPhysicsDecimate {};
+class MarchingSquaresPhysicsDecimate
+{
+public:
+    MarchingSquaresPhysicsDecimate(Transform* transform);
+    ~MarchingSquaresPhysicsDecimate();
+
+    class MSPhysicsBuilder
+    {
+        friend class MarchingSquaresPhysicsDecimate;
+    public:
+        void SetDynamic(bool isDynamic);
+
+        void AddLine(Line line, int x, int y, int lineNo);
+
+        void Build();
+    private:
+        MSPhysicsBuilder(MarchingSquaresPhysicsDecimate* object, int gridWidth, int gridHeight);
+        MarchingSquaresPhysicsDecimate* object;
+
+        Line* lineArray;
+        int gridWidth;
+        int gridHeight;
+
+        int getIndex(int x, int y, int lineNo) {return ((y * gridWidth ) + x) * 2 + lineNo;}
+
+        int lineCount;
+
+        bool isDynamic;
+    };
+
+    MSPhysicsBuilder GetBuilder(int gridWidth, int gridHeight);
+private:
+    Transform* transform;
+
+    b2BodyId bodyId;
+
+};
